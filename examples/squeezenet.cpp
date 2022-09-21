86ff22538c40a2f11602f8c4ef894ecfa303adfb
tpoisonooo <khj.application@aliyun.com>
2022-09-21 15:50:18 +0800

    hack ncnn to support topk and remove concat simd

--- examples/squeezenet.cpp
+++ examples/squeezenet.cpp
@@ -24,72 +24,36 @@
 #include <stdio.h>
 #include <vector>
 
-static int detect_squeezenet(const cv::Mat& bgr, std::vector<float>& cls_scores)
+static int detect_squeezenet(const cv::Mat& bgr)
 {
     ncnn::Net squeezenet;
 
-    squeezenet.opt.use_vulkan_compute = true;
+    squeezenet.opt.use_vulkan_compute = false;
 
     // the ncnn model https://github.com/nihui/ncnn-assets/tree/master/models
-    if (squeezenet.load_param("squeezenet_v1.1.param"))
+    if (squeezenet.load_param("end2end_int8.param"))
         exit(-1);
-    if (squeezenet.load_model("squeezenet_v1.1.bin"))
+    if (squeezenet.load_model("end2end_int8.bin"))
         exit(-1);
 
-    ncnn::Mat in = ncnn::Mat::from_pixels_resize(bgr.data, ncnn::Mat::PIXEL_BGR, bgr.cols, bgr.rows, 227, 227);
+    ncnn::Mat in = ncnn::Mat::from_pixels_resize(bgr.data, ncnn::Mat::PIXEL_BGR, bgr.cols, bgr.rows, 2048, 1024);
 
     const float mean_vals[3] = {104.f, 117.f, 123.f};
     in.substract_mean_normalize(mean_vals, 0);
 
     ncnn::Extractor ex = squeezenet.create_extractor();
 
-    ex.input("data", in);
+    ex.input("input", in);
 
     ncnn::Mat out;
-    ex.extract("prob", out);
-
-    cls_scores.resize(out.w);
-    for (int j = 0; j < out.w; j++)
-    {
-        cls_scores[j] = out[j];
-    }
+    ex.extract("output", out);
 
     return 0;
 }
 
-static int print_topk(const std::vector<float>& cls_scores, int topk)
-{
-    // partial sort topk with index
-    int size = cls_scores.size();
-    std::vector<std::pair<float, int> > vec;
-    vec.resize(size);
-    for (int i = 0; i < size; i++)
-    {
-        vec[i] = std::make_pair(cls_scores[i], i);
-    }
-
-    std::partial_sort(vec.begin(), vec.begin() + topk, vec.end(),
-                      std::greater<std::pair<float, int> >());
-
-    // print topk and score
-    for (int i = 0; i < topk; i++)
-    {
-        float score = vec[i].first;
-        int index = vec[i].second;
-        fprintf(stderr, "%d = %f\n", index, score);
-    }
-
-    return 0;
-}
 
 int main(int argc, char** argv)
 {
-    if (argc != 2)
-    {
-        fprintf(stderr, "Usage: %s [imagepath]\n", argv[0]);
-        return -1;
-    }
-
     const char* imagepath = argv[1];
 
     cv::Mat m = cv::imread(imagepath, 1);
@@ -99,10 +63,7 @@ int main(int argc, char** argv)
         return -1;
     }
 
-    std::vector<float> cls_scores;
-    detect_squeezenet(m, cls_scores);
-
-    print_topk(cls_scores, 3);
+    detect_squeezenet(m);
 
     return 0;
 }
---
+++ src/layer/topk.h
@@ -0,0 +1,25 @@
+// Copyright (c) OpenMMLab. All rights reserved.
+#ifndef LAYER_TOPK_H
+#define LAYER_TOPK_H
+
+#include "layer.h"
+
+namespace ncnn {
+
+class TopK : public Layer {
+ public:
+  TopK();
+  virtual int load_param(const ncnn::ParamDict& pd);
+  virtual int forward(const std::vector<ncnn::Mat>& bottom_blobs, std::vector<ncnn::Mat>& top_blobs,
+                      const ncnn::Option& opt) const;
+
+ public:
+  int axis;
+  int largest;
+  int sorted;
+  int keep_dims;
+};
+
+}  // namespace ncnn
+
+#endif  // LAYER_TOPK_H
